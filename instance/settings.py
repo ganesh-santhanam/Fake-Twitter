58551392de22ad18e55ba7af95e4dc6485c45a7e
Ganesh Santhanam <ganesh.santhanam@outlook.com>
2019-01-24 23:11:15 +0530

    First commit

---
+++ instance/settings.py
@@ -0,0 +1,8 @@
+# Enter in your Pusher app's keys (you can leave the last 2 variables alone).
+PUSHER_APP_ID = '699032'
+PUSHER_KEY = 'e97f445d24adf1f220d6'
+PUSHER_SECRET = 'fd01d4dfe42702e10c13'
+PUSHER_CLUSTER = 'us2'
+
+PUSHER_SSL = True
+PUSHER_AUTH_ENDPOINT = '/api/auth/pusher/'
